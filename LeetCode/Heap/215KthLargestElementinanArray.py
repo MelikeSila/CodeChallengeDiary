9b21c1a39f33179c968317f60d56fe3df9761794
MelikeSila <melikekesskin@gmail.com>
2024-05-05 01:59:09 +0200

    solution with heap but running time is not good

--- LeetCode/Heap/215KthLargestElementinanArray.py
+++ LeetCode/Heap/215KthLargestElementinanArray.py
@@ -1,4 +1,10 @@
-class Solution:
+"""class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort(reverse=True)
-        return nums[k-1]
+        return nums[k-1]"""
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        print(nums)
+        return heapq.nlargest(k, nums)[-1]
