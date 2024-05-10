d3af71e5157c1bea3a6bbd830dce5dfb0b9f9b05
MelikeSila <melikekesskin@gmail.com>
2024-05-10 23:57:01 +0200

    simplified version

--- LeetCode/Math/66PlusOne.py
+++ LeetCode/Math/66PlusOne.py
@@ -2,15 +2,14 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
         i = -1
+        l = len(digits)
 
-        while i != -len(digits)-1:
+        while i != -l-1:
             
             if digits[i] == 9:
-                if i == -len(digits):
-                    digits[i] = 0
+                digits[i] = 0
+                if i == -l:
                     return [1] + digits
-                else:
-                    digits[i] = 0
             else:
                 digits[i] += 1
                 return digits
