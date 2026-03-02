32aafad75f4143f89dc229d949e35b2e2f5be10e
Melike <melikekesskin@gmail.com>
2026-03-02 03:32:35 +0100

    without numpy

--- LeetCode/ArrayString/238_Product _of _Array_Except_Self.py
+++ LeetCode/ArrayString/238_Product _of _Array_Except_Self.py
@@ -2,11 +2,14 @@ class Solution:
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         answer = [0]*len(nums)
-        all_product = 1
-        import numpy as np
+        all_products = 1
 
-        zero_count = len(nums) - np.count_nonzero(nums)
-        print(f"zero count: {zero_count}")
+        zero_count = 0
+        for n in nums:
+            if n == 0:
+                zero_count += 1
+            if zero_count >= 2:
+                break
 
         # if there are more than 1 zero than all answers will be zero
         if zero_count >= 2:
@@ -15,19 +18,30 @@ class Solution:
         elif zero_count == 1:
             for j in range(0, len(nums)):
                 if nums[j] != 0:
-                    all_product *= nums[j]
+                    all_products *= nums[j]
                 else:
                     index = j
-            answer[index] = all_product
+            answer[index] = all_products
             return answer
 
-
-        all_products = 1
-        for n in nums:
-            all_products *= n
+        # O(n/2)
+        n = len(nums)
+        odd = 0
+        if n%2 == 0:
+            a = int(n/2)
+        else:
+            a = int(n/2) + 1
+            odd = 1
         
-        for i in range(0, len(nums)):
-            answer[i] = int(all_products / nums[i])
+        for i in range(0, a):
+            all_products = all_products * nums[i] * nums[-i-1]
+        
+        if odd:
+            all_products = all_products/nums[a-1]
+
+        for i in range(0, a):
+            answer[i] = int(all_products/nums[i])
+            answer[-i-1] = int(all_products / nums [-i-1])
         
         
         return answer
